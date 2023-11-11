2ee20273b63a0c422d56039adf9aac0524664fc7
Šárka Mejzlíková <mejzlikova.s@gmail.com>
2023-11-11 09:19:52 +0100

    Add project files.

---
+++ DomaciUkol_6Lekce_UprStatTextu/Program.cs
@@ -0,0 +1,42 @@
+﻿using DomaciUkol_6Lekce_UprStatTextu;
+using System.Collections.Generic;
+using System.Xml;
+
+string vstupniText = "Ema má maso. Máma má mísu.";
+List<Znak> statistika = new List<Znak>();
+
+foreach (char znak in vstupniText)
+{
+    Znak provereniZnaku = null;
+    for (int i = 0; i < statistika.Count; i++)
+    {
+        if (statistika[i].Pismeno == znak)
+        {
+            provereniZnaku = statistika[i];
+        }
+    }
+
+    if (provereniZnaku != null)
+    {
+        provereniZnaku.Pocet++;
+    }
+    else
+    {
+        statistika.Add(new Znak() { Pismeno = znak, Pocet = 1 });
+    }
+}
+
+foreach (var i in statistika)
+{
+    Console.WriteLine($"{i.Pismeno} - {i.Pocet}");
+}
+
+Console.WriteLine("\nVyskytuje se každý znak právě jednou? " + statistika.All(x => x.Pocet == 1));
+Console.WriteLine("Vyskytuje se nějaký znak alespoň 5x? " + statistika.Any(x => x.Pocet > 5));
+Console.WriteLine("Jaký je minimální počet výskytů jednoho znaku? " + statistika.Min(x => x.Pocet));
+Console.WriteLine("Jaký je maximální počet výskytů jednoho znaku? " + statistika.Max(x => x.Pocet));
+Console.WriteLine("Jaký je průměrný výskyt jednoho znaku? " + Math.Round(statistika.Average(x => x.Pocet),0));
+Console.WriteLine("Jaký je počet použitých znaků? " + statistika.Count());
+Console.WriteLine("Kolik bylo celkem použito znaků? " + statistika.Sum(x => x.Pocet));
+
+Console.ReadLine();
---
+++ DomaciUkol_6Lekce_UprStatTextu/Znak.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomaciUkol_6Lekce_UprStatTextu
+{
+    public class Znak
+    {
+        public char Pismeno { get; set; }
+        public int Pocet { get; set; }
+    }
+}
